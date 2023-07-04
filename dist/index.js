b2c59b0c174bc57fd5614b7da559d03cd1111fad
Bumble <472302877@qq.com>
2023-07-04 16:02:56 +0800

    创建实体 entity，使用 manager 操作实体

--- dist/index.js
+++ dist/index.js
@@ -4,15 +4,32 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
 var _asyncToGenerator2 = _interopRequireDefault(require("@babel/runtime/helpers/asyncToGenerator"));
 var _dataSource = require("./data-source");
+var _Post = require("./entity/Post");
 _dataSource.AppDataSource.initialize().then( /*#__PURE__*/function () {
   var _ref = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee(connection) {
+    var posts, p, posts2;
     return _regenerator["default"].wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
-            console.log(connection);
-            connection.close();
+            _context.next = 2;
+            return connection.manager.find(_Post.Post);
           case 2:
+            posts = _context.sent;
+            console.log(posts);
+            p = new _Post.Post();
+            p.title = 'Post 1';
+            p.content = '这是一个demo';
+            _context.next = 9;
+            return connection.manager.save(p);
+          case 9:
+            _context.next = 11;
+            return connection.manager.find(_Post.Post);
+          case 11:
+            posts2 = _context.sent;
+            console.log(posts2);
+            connection.close();
+          case 14:
           case "end":
             return _context.stop();
         }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import { AppDataSource } from "./data-source"
 import { User } from "./entity/User"
+import {Post} from "./entity/Post";
 
 AppDataSource.initialize().then(async (connection) => {
-    console.log(connection)
+    const posts = await connection.manager.find(Post)
+    console.log(posts)
+    const p = new Post()
+    p.title = 'Post 1'
+    p.content = '这是一个demo'
+    await connection.manager.save(p)
+    const posts2 = await connection.manager.find(Post)
+    console.log(posts2)
     connection.close()
 }).catch(error => console.log(error))
