cefd960df83747217d44e04d328f27be04dd7ef1
Bumble <472302877@qq.com>
2023-07-05 16:24:01 +0800

    清空文件，变为初始项目

--- src/seed.ts
+++ src/seed.ts
@@ -1,13 +1,5 @@
 import {AppDataSource} from "./data-source"
-import {Post} from "./entity/Post";
 
 AppDataSource.initialize().then(async (connection) => {
-  const posts = await connection.manager.find(Post)
-  if (posts.length === 0) {
-    await connection.manager.save([1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(n => {
-      return new Post(`Post ${n}`, `这是第${n}个博客`)
-    }))
-  }
-  console.log('posts 数据填充完了')
   connection.close()
 }).catch(error => console.log(error))
