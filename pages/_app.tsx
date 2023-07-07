b20ab5d185abb2991da27d2a4e0ab62730485f55
Bumble <472302877@qq.com>
2023-07-07 11:26:53 +0800

    修改目录结构

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Layout from '../components/layout';
+import { AppProps } from 'next/app';
+import '../styles/globals.css'
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return <>
+    <Head>
+      <title>我的博客 - Bumble</title>
+      <meta name="viewport"
+            content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no,viewport-fit=cover"/>
+    </Head>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  </>
+}
