c6dc2a49321ef538ae68933981e6f5ce43e0f7ef
Bumble <472302877@qq.com>
2023-07-12 15:59:18 +0800

    增加 BeforeInsert

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,7 +1,16 @@
-import {Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn} from "typeorm"
+import {
+  BeforeInsert,
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn
+} from "typeorm"
 import {Post} from "./Post";
 import {Comment} from "./Comment";
 import {handleGetRepository} from "@/lib/handleGetRepository";
+import md5 from "md5";
 
 @Entity('users')
 export class User {
@@ -56,5 +65,8 @@ export class User {
   hasErrors(){
     return !!Object.values(this.errors).find(v => v.length > 0)
   }
-
+  @BeforeInsert()
+  generatePasswordDigest(){
+    this.passwordDigest = md5(this.password) as string
+  }
 }
