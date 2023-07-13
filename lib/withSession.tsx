dd244ac1da0c46eb49b115d629170be72d0047dd
Bumble <472302877@qq.com>
2023-07-13 16:25:54 +0800

    使用 .env.local 设置环境变量

--- lib/withSession.tsx
+++ lib/withSession.tsx
@@ -3,7 +3,7 @@ import {withIronSession} from 'next-iron-session';
 export function withSession(handler) {
   return withIronSession(handler, {
     // password: process.env.SECRET_COOKIE_PASSWORD,
-    password: 'c2a85490-cc60-4f21-94e8-8dc5dd3220da',
+    password: process.env.SECRET,
     cookieName: 'blog',
     cookieOptions: {secure: false}
   });
