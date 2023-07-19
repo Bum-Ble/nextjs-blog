87bb70d422cb24a715cdc01be0403950f3eb8ff2
Bumble <472302877@qq.com>
2023-07-19 10:30:12 +0800

    抽离 usePager 自定义 hook

---
+++ hooks/usePager.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+import _ from "lodash"
+
+type Options = {
+  page: number
+  totalPage: number,
+  urlMaker?:(n:number) => string
+}
+const defaultUrlMaker = (n: number) => `?page=${n}`
+
+export const usePager = (options: Options) => {
+  const {page, totalPage, urlMaker: _urlMaker} = options
+  const urlMaker = _urlMaker || defaultUrlMaker
+  const numbers = []
+  numbers.push(1)
+  for (let i = page - 3; i <= page + 3; i++) {
+    numbers.push(i)
+  }
+  numbers.push(totalPage)
+  const pageNumbers = _.uniq(numbers).sort().filter(n => n >= 1 && n <= totalPage)
+    .reduce((result, n) => {
+      return n - (result[result.length - 1] || 0) === 1 ? result.concat(n) : result.concat(-1, n)
+    }, [])
+  const pager = (
+    <div>
+      {page !== 1 && <Link href={urlMaker(page-1)}>上一页</Link>}
+      {
+        pageNumbers.map((n, i) => n === -1 ?
+          <span className='pageBtn' key={i}>...</span>
+          : <Link key={i} href={urlMaker(n)}><a className='pageBtn'>{n}</a></Link>
+        )
+      }
+      {page < totalPage && <Link href={urlMaker(page+1)}>下一页</Link>}
+      第 {page}/{totalPage} 页
+      <style jsx>{`
+        .pageBtn{
+          margin: 0 8px;
+        }
+      `}
+      </style>
+    </div>
+  )
+  return {pager}
+}
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import {GetServerSideProps, GetServerSidePropsContext, NextPage} from "next";
 import {handleGetRepository} from "@/lib/handleGetRepository";
 import {Post} from "@/src/entity/Post";
+import {usePager} from "@/hooks/usePager";
 
 type Props = {
   posts: PostType[]
@@ -11,7 +12,8 @@ type Props = {
   totalPage: number
 }
 const PostsIndex: NextPage<Props> = (props) => {
-  const {posts} = props
+  const {posts, page, totalPage} = props
+  const {pager} = usePager({page, totalPage})
   return (
     <div>
       <h1>文章列表</h1>
@@ -24,9 +26,7 @@ const PostsIndex: NextPage<Props> = (props) => {
           </div>)
       }
       <footer>
-        第 {props.page}/{props.totalPage} 页，共 {props.count} 篇文章
-        &nbsp;{ props.page !== 1 && <Link href={`?page=${props.page - 1}`}>上一页</Link> }
-        { props.page < props.totalPage && <Link href={`?page=${props.page + 1}`}>下一页</Link> }
+        {pager}
       </footer>
     </div>
   )
@@ -35,7 +35,7 @@ export default PostsIndex
 
 export const getServerSideProps: GetServerSideProps = async (context: GetServerSidePropsContext) => {
   const page  = parseInt(context.query.page as string) || 1
-  const pageSize = 3;
+  const pageSize = 1;
   const skip = (page - 1) * pageSize;
   const PostRepository = await handleGetRepository(Post)
   const [posts, count] = await PostRepository.findAndCount({
