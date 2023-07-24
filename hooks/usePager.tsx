2f67aa4fef4f7cb27180aacc5082e4aaad1ef01d
Bumble <472302877@qq.com>
2023-07-24 15:51:06 +0800

    分页 添加css样式

--- hooks/usePager.tsx
+++ hooks/usePager.tsx
@@ -17,24 +17,35 @@ export const usePager = (options: Options) => {
     numbers.push(i)
   }
   numbers.push(totalPage)
-  const pageNumbers = _.uniq(numbers).sort().filter(n => n >= 1 && n <= totalPage)
+  const sortNumbers = _.uniq(numbers).sort(function(a, b) { return a - b; });
+  const pageNumbers = sortNumbers.filter(n => n >= 1 && n <= totalPage)
     .reduce<number[]>((result: number[], n: number) => {
       return n - (result[result.length - 1] || 0) === 1 ? [...result, n] : [...result, -1, n]
     }, [])
   const pager = (
     <div>
-      {page !== 1 && <Link href={urlMaker(page-1)}>上一页</Link>}
+      {page !== 1 && <Link href={urlMaker(page-1)}><span className="pageBtn textColor">上一页</span></Link>}
       {
         pageNumbers.map((n, i) => n === -1 ?
-          <span className='pageBtn' key={i}>...</span>
-          : <Link key={i} href={urlMaker(n)}><a className='pageBtn'>{n}</a></Link>
+          <span className='textColor' key={i}>...</span>
+          : <Link key={i} href={urlMaker(n)}>
+              <span className={`pageBtn textColor ${n === page ? 'textActive' : ''}`}>{n}</span>
+            </Link>
         )
       }
-      {page < totalPage && <Link href={urlMaker(page+1)}>下一页</Link>}
-      &nbsp;第 {page}/{totalPage} 页
+      {page < totalPage && <Link href={urlMaker(page+1)}><span className="pageBtn textColor">下一页 </span></Link>}
+      <span className="textColor">第 {page}/{totalPage} 页</span>
       <style jsx>{`
         .pageBtn{
           margin: 0 8px;
+          cursor: pointer;
+        }
+        .textColor{
+          //color: #f5f5f5;
+        }
+        .textActive{
+          font-weight: bold;
+          font-size: 18px;
         }
       `}
       </style>
