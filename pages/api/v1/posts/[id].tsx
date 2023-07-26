da46735341685d528ce9ee115e7bc8f4e9490c1a
Bumble <472302877@qq.com>
2023-07-26 11:25:50 +0800

    增加删除操作

--- pages/api/v1/posts/[id].tsx
+++ pages/api/v1/posts/[id].tsx
@@ -5,24 +5,31 @@ import {withSession} from "@/lib/withSession";
 
 const Posts: NextApiHandler = withSession(async (req, res) => {
   const {id} = req.query
+  const PostRepository = await handleGetRepository(Post)
+  const post = await PostRepository.findOneBy({id}) as PostType
+  const authorId = post.authorId
+  const {title, content} = req.body
+  const user = req.session.get('currentUser')
+  if (!user){
+    res.statusCode = 401
+    res.end()
+    return
+  }else if (user.id !== authorId){
+    res.status(405).json({message: '只允许操作本人的文章'})
+    return
+  }
   if (req.method === 'PATCH'){
-    const {title, content, authorId} = req.body
-    const user = req.session.get('currentUser')
-    if (!user){
-      res.statusCode = 401
-      res.end()
-      return
-    }else if (user.id !== authorId){
-      res.statusCode = 405
-      res.end('只允许修改本人的文章')
-      return
-    }
-    const PostRepository = await handleGetRepository(Post)
-    const post = await PostRepository.findOneBy({id}) as PostType
     post.title = title
     post.content = content
     await PostRepository.save(post)
     res.json(post)
+  }else if (req.method === 'DELETE'){
+    try{
+      await PostRepository.remove(post)
+      res.end()
+    }catch (error){
+      res.status(500).json({ message: '删除操作失败' })
+    }
   }
 })
 export default Posts
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -2,21 +2,37 @@ import {GetServerSideProps, NextPage} from "next";
 import {handleGetRepository} from "@/lib/handleGetRepository";
 import {Post} from "@/src/entity/Post";
 import Link from "next/link";
+import axios from "axios";
+import {useRouter} from "next/router";
 
 const marked = require('marked');
 
 type Props = {
   post: PostType
 }
-const postsShow: NextPage<Props> = (props) => {
+const PostsShow: NextPage<Props> = (props) => {
   const {post} = props
+  const router = useRouter()
+  const handleDelete = () => {
+    const result = window.confirm('确定要删除文章吗？')
+    if (result){
+      axios.delete(`/api/v1/posts/${post.id}`).then(() => {
+        alert('删除成功')
+        router.push('/posts')
+      }, (error) => {
+        window.alert(error.response.data.message)
+      })
+    }
+  }
   return (
     <>
       <div className="wrapper">
         <div className="content">
           <div className="link">
-            <Link  href={`/posts/${post.id}/edit`}><a className="link-text">编辑</a></Link>
-            <Link  href="/posts"><a className="link-text">返回列表</a></Link>
+            <Link href={`/posts/${post.id}/edit`}><a className="link-text">编辑</a></Link>
+            <Link href="/posts"><a className="link-text">返回列表</a></Link>
+            <button className="deleteBtn link-text" onClick={handleDelete}>删除文章</button>
+
           </div>
           <h1>{post.title}</h1>
           <article className="markdown-body" dangerouslySetInnerHTML={{__html: marked.parse(post.content)}}/>
@@ -31,10 +47,24 @@ const postsShow: NextPage<Props> = (props) => {
           .link{
             display: flex;
             justify-content: flex-end;
+            align-items: center;
           }
           .link-text{
-            margin-left: 10px;
+            margin: 0 6px;
             text-decoration: none;
+            color: #0056b3;
+            
+          }
+          .link-text:hover{
+            color: #007bff;
+          }
+          .deleteBtn{
+            padding: 0;
+            border: none;
+            background-color: #fff;
+            font-size: 16px;
+            cursor: pointer;
+          
           }
 
           .content {
@@ -56,7 +86,7 @@ const postsShow: NextPage<Props> = (props) => {
     </>
   )
 }
-export default postsShow
+export default PostsShow
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const {params} = context;
