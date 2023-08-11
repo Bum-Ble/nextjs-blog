ef853e740816188f6501a6f84ef325a493526bba
Bumble <472302877@qq.com>
2023-08-11 11:30:01 +0800

    调试 process.env.SECRET 改为 process.env.NEXT_PUBLIC_SECRET

--- lib/session.tsx
+++ lib/session.tsx
@@ -1,7 +1,6 @@
-console.log(process.env.SECRET!.toString())
 export const sessionOptions = {
   cookieName: "blog",
-  password: process.env.SECRET!.toString(),
+  password: process.env.NODE_ENV === 'development' ? process.env.SECRET!.toString() : process.env.NEXT_PUBLIC_SECRET!.toString(),
   // secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
   cookieOptions: {
     secure: false,
